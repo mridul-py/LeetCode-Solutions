3ab76dbc84af905c41142ef73e91228283776505
Mridul Gupta <mridul22gupta@gmail.com>
2024-07-18 20:16:53 +0530

    Create Add Two Numbers.java

---
+++ Add Two Numbers.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+ import java.util.*;
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode result= new ListNode();
+        ListNode head = result;
+        int total= 0;
+        int carry =0;
+        int i=0;
+        while(l1!=null||l2!=null||carry!=0){
+            total=carry;
+            if(l1!=null){
+                total+=l1.val;
+                l1=l1.next;
+            }
+            if(l2!=null){
+                total+=l2.val;
+                l2=l2.next;
+            }
+            carry=total/10;
+            result.next= new ListNode(total%10);
+            result=result.next;
+
+        }
+        return head.next;
+    }
+}
