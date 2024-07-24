607505a6bb5f7e1ffc7cc27cc26288d516866eaf
Mridul Gupta <mridul22gupta@gmail.com>
2024-07-24 09:39:21 +0530

    Create remove Element.java

---
+++ remove Element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+
+        int k=0;
+        for (int i: nums){
+            if( i!= val){
+                nums[k]=i;
+                k++;
+            }
+        }
+return k;
+    }
+}
