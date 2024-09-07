1207013f8a4d881d166085e45417cbf673214525
Mridul Gupta <mridul22gupta@gmail.com>
2024-09-07 23:36:34 +0530

    Create Online Stock span.java

---
+++ Online Stock span.java
@@ -0,0 +1,21 @@
+class StockSpanner {
+    Stack<Integer> st;
+    ArrayList<Integer>  list;
+
+    public StockSpanner() {
+        st=new Stack<>();
+        list=new ArrayList<>();   
+    }
+    
+    public int next(int price) {
+        list.add(price);
+        while(!st.isEmpty()&& list.get(st.peek())<=price){
+            st.pop();
+        }
+        int previousGreatestIndex= st.isEmpty()?-1:st.peek();
+        int ans=(list.size()-1)-previousGreatestIndex;
+        st.push(list.size()-1);
+        return ans;
+    }
+}
+
