5ffc22c2db25b0157b8847ebbffaf0a16658daad
Mridul Gupta <mridul22gupta@gmail.com>
2024-11-14 18:34:44 +0530

    Create  H index II .py

---
+++ H index II .py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+        start, end = 0, n
+        
+        while start < end:
+            mid = (start + end) // 2
+            if citations[mid] >= n - mid:
+                end = mid
+            else:
+                start = mid + 1
+        
+        return n - start
