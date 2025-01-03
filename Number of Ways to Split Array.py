52a6e4f0f8cbfbe04c5a78bb713a2a48221bf8c9
Mridul Gupta <mridul22gupta@gmail.com>
2025-01-03 17:02:18 +0530

    Create Number of Ways to Split Array.py

---
+++ Number of Ways to Split Array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def waysToSplitArray(self, nums: List[int]) -> int:
+        total_sum = sum(nums)
+        left_sum = 0
+        count = 0
+        
+        for i in range(len(nums) - 1):  # Avoid splitting at the last index
+            left_sum += nums[i]
+            right_sum = total_sum - left_sum
+            if left_sum >= right_sum:
+                count += 1
+        
+        return count
